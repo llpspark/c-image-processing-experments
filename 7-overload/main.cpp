31aedbfd2ee7f4743b62ebb80d49927423d9536c
Lipeng Liu <llpspark@outlook.com>
2017-01-16 13:44:19 +0800

    Add files via upload

---
+++ 7-overload/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+using namespace std;
+#include "highgui.h"
+#include "Image.h"
+
+int main(int argc, char* argv[])
+{
+
+	Matrix ma,mb,mc,md,me;
+	ma.Ones(4,4);
+	mb.Ones(4,4);;
+	//mb.Ones(4,4);
+	/*mc=ma-mb;
+	md=--ma;*/
+	/*md=ma--;
+	md=ma--;*/
+	//以下两句是等同的
+	//mc=operator+(ma,3);
+   // mc=ma+3;
+	//以下两句是等同的
+	//mc=operator*(ma,3); 
+   // mc=ma*3;
+	//以下两句是等同的
+	//mc.operator = (ma*mb);
+	//mc=ma*mb;e
+
+	Image img("images//scene1_bg.jpg"),img1;
+	img.Show("original");
+	//img1=-img;	
+    //img1.Show("flip");
+	/*img.gray2bw(90); 
+	img.Show("gra2bw");*/
+	/*img1=img+20;
+	img1.Show("micro-plus");*/
+	img1=img-30;
+	img1.Show("micro-minus");
+
+
+
+	
+	//mb=ma.Row(5);-----------------以下均是继承实验中的调试代码
+	//mb=ma.Column(5);
+	//ma.Transpose();
+	//double c=++(ma.At(2,2));
+	//ma.Reshape(2,8);
+	//bool A;
+	//A=ma.IsEmpty();
+	//A=ma.IsSquare();
+	//ma.CopyTo(mb);
+	//ma.Mul(5);
+	//ma.Cat(mb,4);
+
+	//mc=Add(ma,mb);
+	//mc= Sub(ma,mb);
+	 //Swap(ma,mb);
+	//Matrix m(16, 1);
+	//for (int i = 0; i < 16; i++)
+	//{
+	//	m.At(i, 0) = i%256;
+		//std::cout<<++(m.At(i, 0));
+   	 // m.Set(i,0,i%256);
+	//}
+	//m.Reshape(4, 4);
+	//for(int i=0;i<4;i++)        
+	//		{
+	//		for(int j=0;j<4;j++)
+	//		{
+	//		std::cout<<m.At(i,j);
+	//		}
+	//		std::cout<<endl;
+	//		}
+            
+	//Image img("Fruits.jpg");
+	/*Image img;
+	img.Read("Images\\Fruits.jpg");*/
+	//img.Write("Images\\CopeFruits.jpg");
+	//img.Show("image");
+	//img.Flip(0);
+	//img.Show("image");
+	//Image img(m);
+	//img.Resize(666,666);
+	//img.Show("image");
+	//img.Cut(222,222,444,444);
+	//img.Show("image");
+	//double a=img.Mean();
+	//double b=img.Variance();
+	/*img.Rotate(36);
+    img.Show("image");
+*/
+
+
+	//Matrix d = img.MajorDiagonal();
+	//Image *p = new Image(d);
+	//p->Show("pimage");
+	//p->Reshape(30,30);
+ //   p->Show("pimagereshaped");
+	//delete p;
+	//p = NULL;
+
+
+	//Image im("Images\\Fruits.jpg");
+	//im.Resize(200, 200);
+	//im.Show("resized image");
+	//im.Rotate(30);
+	//im.Show("rotated image");
+
+	//Image img1("Images\\Fruits.jpg"); 
+	//img1.Show("image2");
+	//Image img2("Images\\word.jpg"); 
+	//img2.Show("image1");
+	//两图片相加
+	/*Image img_add(Add(img1, img2)) ;
+	img_add.Show("add");*/
+
+	//两图片相减
+	/*Image img_sub(Sub(img1, img2));
+	img_sub.Show("sub");
+*/
+	//图片的转置
+	//Image img3("Images\\lena.jpg");
+	//img3.Show("yuantu");
+	//img3.Transpose();
+	//img3.Show("transpose");
+
+	////在右边拼接图片；
+	//Image img4("Images\\airplane.jpg"); 
+	//Image img5("Images\\baboon.jpg");
+	//img4.Cat(img5,4);
+	//img4.Show("catright");
+	
+	return 0;
+}
